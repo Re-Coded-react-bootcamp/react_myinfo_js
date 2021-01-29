09827dac274021a3292408bdf0622c12650031c2
Ravyar19 <captainravyar@gmail.com>
2021-01-29 19:47:48 +0300

    master

---
+++ src/componets/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./MyInfo.css"
+
+export default function MyInfo() {
+    return (
+        <div className="main">
+            <h1>Ravyar Aram</h1>
+            <p>Hello there !!! My name is Ravyar and i'm 20 years old</p>
+            <ul>
+                <li>Las Vegas</li>
+                <li>Singapore</li>
+                <li>indonesia</li>
+                <li>France</li>
+            </ul>
+        </div>
+    )
+}
